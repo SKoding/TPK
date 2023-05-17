a51c0920f2f5ec53ef9c9fdc2c2aa4b2830bf397
SKoding <mundiasamuel4@gmail.com>
2023-05-17 21:02:56 +0300

    trees commented

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from apps import home, trees, upload  # import your app modules here
+from apps import home, upload  # import your app modules here
 
 st.set_page_config(page_title="Tree Pact Kenya", layout="wide")
 
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Tree Pact Kenya", layout="wide")
 
 apps = [
     {"func": home.app, "title": "Home", "icon": "house"},
-    {"func": trees.app, "title": "Trees", "icon": "map"},
+    # {"func": trees.app, "title": "Trees", "icon": "map"},
     {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
 ]
 
