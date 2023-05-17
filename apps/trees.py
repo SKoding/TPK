3f293c96e6ea6d6f58845fb8788a2a4e7fc31db1
SKoding <mundiasamuel4@gmail.com>
2023-05-17 22:02:11 +0300

    trees

--- apps/trees.py
+++ apps/trees.py
@@ -1,26 +1,24 @@
+import ee
 import streamlit as st
-from streamlit_folium import folium_static
-import folium
+import geemap.foliumap as geemap
 
-"# streamlit-geemap"
 
-with st.echo():
-    import streamlit as st
-    from streamlit_folium import folium_static
-    import ee
-    import geemap.eefolium as geemap
-
-    m = geemap.Map()
-
-    dem = ee.Image('USGS/SRTMGL1_003')
-    # Set visualization parameters.
+def app():
+    # Create an interactive map
+    Map = geemap.Map(plugin_Draw=True, Draw_export=False)
+    # Add a basemap
+    Map.add_basemap("TERRAIN")
+    # Retrieve Earth Engine dataset
+    dem = ee.Image("USGS/SRTMGL1_003")
+    # Set visualization parameters
     vis_params = {
-    'min': 0,
-    'max': 4000,
-    'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
-
-    m.addLayer(dem, vis_params, 'DEM')
-    m.addLayerControl()
-
-    # call to render Folium map in Streamlit
-    folium_static(m)
+        "min": 0,
+        "max": 4000,
+        "palette": ["006633", "E5FFCC", "662A00", "D8D8D8", "F5F5F5"],
+    }
+    # Add the Earth Engine image to the map
+    Map.addLayer(dem, vis_params, "SRTM DEM", True, 0.5)
+    # Add a colorbar to the map
+    Map.add_colorbar(vis_params, label="Elevation (m)")
+    # Render the map using streamlit
+    Map.to_streamlit()
