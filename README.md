3cf7dff1c5d9bda91e821d46afd527e7f2ba912d
SKoding <mundiasamuel4@gmail.com>
2023-05-17 11:22:53 +0300

    changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# TPK
-A StreamLit application
+# TREE PACT KENYA
+## A GIS-based solution to Tackling Climate Change.
+Tree Pact Kenya Proposes to use GIS in the identification,planning,implementation and monitoring of trees in an Aim to plant 15bn Trees By 2030.   
---
+++ streamlit_app.py
@@ -0,0 +1,92 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from apps import home, trees, upload  # import your app modules here
+
+st.set_page_config(page_title="Tree Pact Kenya", layout="wide")
+
+# A dictionary of apps in the format of {"App title": "App icon"}
+# More icons can be found here: https://icons.getbootstrap.com
+
+apps = [
+    {"func": home.app, "title": "Home", "icon": "house"},
+    {"func": trees.app, "title": "Trees", "icon": "map"},
+    {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
+]
+
+titles = [app["title"] for app in apps]
+titles_lower = [title.lower() for title in titles]
+icons = [app["icon"] for app in apps]
+
+params = st.experimental_get_query_params()
+
+if "page" in params:
+    default_index = int(titles_lower.index(params["page"][0].lower()))
+else:
+    default_index = 0
+
+with st.sidebar:
+    selected = option_menu(
+        "Main Menu",
+        options=titles,
+        icons=icons,
+        menu_icon="cast",
+        default_index=default_index,
+    )
+
+    st.sidebar.title("About")
+    st.sidebar.info(
+        """
+        This App shows Degraded Forest areas in Kenya that can be considered for reafforestation efforts. Tree Pact Kenya Aims at using GIS to identify, plan and implement Tree planting across the country. 
+    """
+    )
+
+for app in apps:
+    if app["title"] == selected:
+        app["func"]()
+        break
+import streamlit as st
+from streamlit_option_menu import option_menu
+from apps import home, heatmap, upload  # import your app modules here
+
+st.set_page_config(page_title="Tree Pact Kenya", layout="wide")
+
+# A dictionary of apps in the format of {"App title": "App icon"}
+# More icons can be found here: https://icons.getbootstrap.com
+
+apps = [
+    {"func": home.app, "title": "Home", "icon": "house"},
+    {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
+    {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
+]
+
+titles = [app["title"] for app in apps]
+titles_lower = [title.lower() for title in titles]
+icons = [app["icon"] for app in apps]
+
+params = st.experimental_get_query_params()
+
+if "page" in params:
+    default_index = int(titles_lower.index(params["page"][0].lower()))
+else:
+    default_index = 0
+
+with st.sidebar:
+    selected = option_menu(
+        "Main Menu",
+        options=titles,
+        icons=icons,
+        menu_icon="cast",
+        default_index=default_index,
+    )
+
+    st.sidebar.title("About")
+    st.sidebar.info(
+        """
+        This App shows Degraded Forest areas in Kenya that can be considered for reafforestation efforts. Tree Pact Kenya Aims at using GIS to identify, plan and implement Tree planting across the country. 
+    """
+    )
+
+for app in apps:
+    if app["title"] == selected:
+        app["func"]()
+        break
